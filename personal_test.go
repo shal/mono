7ed8d5a16b48eb141006809df3ebd65aa62d1c8a
Artem <kolomytsev1996@gmail.com>
2020-07-10 21:21:36 +0300

    add basic test for transactions

--- personal_test.go
+++ personal_test.go
@@ -1,7 +1,10 @@
 package mono
 
 import (
+	"context"
+	"net/http"
 	"testing"
+	"time"
 )
 
 func TestNewPersonal(t *testing.T) {
@@ -40,23 +43,80 @@ func TestPersonal_User(t *testing.T) {
 
 //}
 
-//func TestPersonal_Transactions(t *testing.T) {
-//	transactions := []Transaction{
-//		{
-//			ID:              "ZuHWzqkKGVo=",
-//			Time:            1554466347,
-//			Description:     "Покупка щастя",
-//			MCC:             7997,
-//			Hold:            false,
-//			Amount:          -95000,
-//			OperationAmount: -95000,
-//			CurrencyCode:    980,
-//			CommissionRate:  0,
-//			CashBackAmount:  19000,
-//		},
-//	}
-//
-//}
+func TestPersonal_Transactions(t *testing.T) {
+	client := NewPersonal("Success")
+	mockResponse := `
+[
+    {
+        "id": "zxcvtyuioasdfghj",
+        "time": 1593959742,
+        "description": "5************6",
+        "mcc": 4829,
+        "amount": -128200,
+        "operationAmount": -128200,
+        "currencyCode": 980,
+        "commissionRate": 0,
+        "cashbackAmount": 0,
+        "balance": 0,
+        "hold": true
+    },
+    {
+        "id": "qwertyuioasdfghj",
+        "time": 1593936446,
+        "description": "Киевстар\n+380688888888",
+        "mcc": 4814,
+        "amount": -20000,
+        "operationAmount": -20000,
+        "currencyCode": 980,
+        "commissionRate": 0,
+        "cashbackAmount": 0,
+        "balance": 128200,
+        "hold": true
+    }
+]`
+	expectedTransactions := []Transaction{
+		{
+			ID:              "zxcvtyuioasdfghj",
+			Time:            1593959742,
+			Description:     "5************6",
+			MCC:             4829,
+			Hold:            true,
+			Amount:          -128200,
+			OperationAmount: -128200,
+			CurrencyCode:    980,
+			CommissionRate:  0,
+			CashBackAmount:  0,
+			Balance:         0,
+		},
+		{
+			ID:              "qwertyuioasdfghj",
+			Time:            1593936446,
+			Description:     "Киевстар\n+380688888888",
+			MCC:             4814,
+			Hold:            true,
+			Amount:          -20000,
+			OperationAmount: -20000,
+			CurrencyCode:    980,
+			CommissionRate:  0,
+			CashBackAmount:  0,
+			Balance:         128200,
+		},
+	}
+	srv, _ := FakeServer(mockResponse, http.StatusOK)
+	defer srv.Close()
+	client.SetBaseURL(srv.URL)
+
+	actual, err := client.Transactions(context.TODO(), "acc", time.Now(), time.Now())
+
+	if err != nil {
+		t.Fatalf("expected error: nil, actual error: %v", err)
+	}
+	for i, v := range expectedTransactions {
+		if actual[i] != v {
+			t.Fatalf("expected transactions[%v]: %v, actual transactions[%v]: %v", i, expectedTransactions[i], i, actual[i])
+		}
+	}
+}
 
 //func TestPersonal_SetWebHook(t *testing.T) {
 //
